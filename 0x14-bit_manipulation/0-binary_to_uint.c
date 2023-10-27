68935d43daeb85fab1ca16922f87235c670fad4c
joseeboi <joseeboi10@gmail.com>
2023-10-27 04:38:38 -0700

    git bit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * binary_to_unit - a program that convert binary numbers
+ * to intergers/decimal numbers
+ *
+ * @b: a character pointer to a string containing the
+ * binary numbers
+ *
+ * Return: the converted decimal (outpuit)
+*/
+unsigned int binary_to_unit(const char *b)
+{
+	unsigned int len, i, j, sum, pow;
+	int base;
+
+	base = 2;
+	sum = 0;
+	pow = 1;
+
+	if (b == NULL)
+		return (0);
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+	{
+		return (b[0] - 48);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+		for (j = len - 1; j > 0; j--)
+		pow = pow * base;
+		sum = sum + (pow * (b[i] - 48));
+		len--;
+		pow = 1;
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+my readme file bit manipulation
