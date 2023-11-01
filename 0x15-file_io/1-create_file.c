945f01d23076dfed01c5bdc7c9648a8302501c7f
joseeboi <joseeboi10@gmail.com>
2023-11-01 03:20:37 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,28 +11,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d;
+	int f_d;
 	int nletters;
 	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	f_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file_d == -1)
+	if (f_d == -1)
 		return (-1);
 
 	if (!text_content)
-		text_content != NULL;
+		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
-	rwr = write(file_d, text_content, nletters);
+	rwr = write(f_d, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
-	close(file_d);
+
+	close(f_d);
 
 	return (1);
 }
