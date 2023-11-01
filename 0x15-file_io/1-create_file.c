b642b1f5d2832c5702bdcf94a37aa213585534b1
joseeboi <joseeboi10@gmail.com>
2023-11-01 00:31:54 -0700

    3-cp.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		text_content = NULL;
+		text_content != NULL;
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
